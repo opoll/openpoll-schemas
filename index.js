9819fdb2fe9c532ce69c1457fb3ecfd7e4a5532b
Ruben Rutten <ruben@rubenrutten.nl>
2018-03-05 13:58:15 +0100

    Fixed IDs in schemas, added index.js, removed useless dependencies

---
+++ index.js
@@ -0,0 +1,32 @@
+const promisify = require("util").promisify;
+const fs = require("fs");
+const readdir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
+const path = require("path");
+const validator = new (require("jsonschema").Validator);
+
+module.exports.validator = validator;
+module.exports.initialized = false;
+
+const walk = async (directory) => {
+	let schemas = {};
+	let files = await readdir(directory);
+
+	for(let file of files) {
+		let absolutePath = path.join(directory, file);
+		if ((await stat(absolutePath)).isDirectory()) {
+			schemas[file] = await walk(absolutePath);
+		} else if (path.extname(file) == ".json") {
+			let schema = require(absolutePath);
+			schemas[path.basename(file, ".json")] = schema;
+			validator.addSchema(schema);
+		}
+	};
+
+	return schemas;
+};
+
+walk(path.resolve(__dirname, "schemas")).then(schemas => {
+	module.exports.schemas = schemas;
+	module.exports.initialized = true;
+});
